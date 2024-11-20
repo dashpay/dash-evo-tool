d45990daf84e18d7ee3c3f17aa4fb5933d8ad64b
pauldelucia <pauldelucia2@gmail.com>
2024-11-20 13:46:19 +0700

    fix: remove duplicate testnet nodes in env and remove unused env in exe dir

--- src/app_dir.rs
+++ src/app_dir.rs
@@ -45,16 +45,13 @@ pub fn copy_env_file_if_not_exists() {
     let app_data_dir =
         app_user_data_dir_path().expect("Failed to determine application data directory");
     let env_file_in_app_dir = app_data_dir.join(".env".to_string());
-    if env_file_in_app_dir.exists() && env_file_in_app_dir.is_file() {
-    } else {
+    if !env_file_in_app_dir.exists() || !env_file_in_app_dir.is_file() {
         let env_example_file_in_exe_dir = PathBuf::from(".env.example");
         if env_example_file_in_exe_dir.exists() && env_example_file_in_exe_dir.is_file() {
             fs::copy(&env_example_file_in_exe_dir, env_file_in_app_dir)
                 .expect("Failed to copy main net env file");
         } else {
-            let env_file_in_exe_dir = PathBuf::from(".env");
-            fs::copy(&env_file_in_exe_dir, env_file_in_app_dir)
-                .expect("Failed to copy main net env file");
+            panic!("Failed to create environment variables: failed to find .env.example file in the executable directory");
         }
     }
 }

40cbc1212f3351280be21fef27979d6870f237ff
pauldelucia <pauldelucia2@gmail.com>
2024-12-20 15:42:23 -0500

    almost done

--- src/ui/components/contract_chooser_panel.rs
+++ src/ui/components/contract_chooser_panel.rs
@@ -1,24 +1,31 @@
 use crate::app::AppAction;
 use crate::context::AppContext;
-use dash_sdk::dpp::data_contract::accessors::v0::DataContractV0Getters;
+use crate::model::qualified_contract::QualifiedContract;
 use dash_sdk::dpp::data_contract::document_type::accessors::DocumentTypeV0Getters;
+use dash_sdk::dpp::data_contract::document_type::Index;
+use dash_sdk::dpp::data_contract::{
+    accessors::v0::DataContractV0Getters, document_type::DocumentType,
+};
 use dash_sdk::dpp::platform_value::string_encoding::Encoding;
-use egui::{Context, Frame, Margin, SidePanel};
+use egui::{Color32, Context, Frame, Margin, RichText, SidePanel};
 use std::sync::Arc;
+
 pub fn add_contract_chooser_panel(
     ctx: &Context,
     current_search_term: &mut String,
     app_context: &Arc<AppContext>,
+    selected_data_contract: &mut QualifiedContract,
+    selected_document_type: &mut DocumentType,
+    selected_index: &mut Option<Index>,
+    document_query: &mut String,
 ) -> AppAction {
     let action = AppAction::None;
 
-    // Fetch contracts from the app context
     let contracts = app_context.get_contracts(None, None).unwrap_or_else(|e| {
         eprintln!("Error fetching contracts: {}", e);
         vec![]
     });
 
-    // Filter the contracts based on the search term
     let filtered_contracts: Vec<_> = contracts
         .iter()
         .filter(|contract| {
@@ -40,42 +47,119 @@ pub fn add_contract_chooser_panel(
                 .inner_margin(Margin::same(10.0)),
         )
         .show(ctx, |ui| {
-            // Search bar at the top
             ui.horizontal(|ui| {
                 ui.label("Search:");
                 ui.text_edit_singleline(current_search_term);
             });
 
-            ui.separator(); // Separator below the search bar
+            ui.separator();
 
-            // Display filtered contracts with nested document types and indexes
             ui.vertical(|ui| {
                 for contract in filtered_contracts {
+                    let is_selected_contract = *selected_data_contract == *contract;
+
                     let name_or_id = contract
                         .alias
                         .clone()
                         .unwrap_or(contract.contract.id().to_string(Encoding::Base58));
 
-                    // Expandable contract section
-                    ui.collapsing(name_or_id, |ui| {
-                        // Loop over the document types in the contract
+                    let contract_header_text = if is_selected_contract {
+                        RichText::new(name_or_id).color(Color32::from_rgb(21, 101, 192))
+                    } else {
+                        RichText::new(name_or_id)
+                    };
+
+                    ui.collapsing(contract_header_text, |ui| {
                         for (doc_name, doc_type) in contract.contract.document_types() {
-                            // Expandable section for each document type
-                            ui.collapsing(doc_name, |ui| {
-                                // Loop over the indexes in the document type
-                                for index in doc_type.indexes().values() {
-                                    ui.label(format!("Index: {}", index.name));
-                                    ui.indent("index_properties", |ui| {
+                            let is_selected_doc_type = *selected_document_type == *doc_type;
+
+                            let doc_type_header_text = if is_selected_doc_type {
+                                RichText::new(doc_name.clone())
+                                    .color(Color32::from_rgb(21, 101, 192))
+                            } else {
+                                RichText::new(doc_name.clone())
+                            };
+
+                            let doc_resp = ui.collapsing(doc_type_header_text, |ui| {
+                                // Display indexes as collapsible items
+                                for (index_name, index) in doc_type.indexes() {
+                                    let is_selected_index = *selected_index == Some(index.clone());
+
+                                    let index_header_text = if is_selected_index {
+                                        RichText::new(format!("Index: {}", index_name))
+                                            .color(Color32::from_rgb(21, 101, 192))
+                                    } else {
+                                        RichText::new(format!("Index: {}", index_name))
+                                    };
+
+                                    let index_resp = ui.collapsing(index_header_text, |ui| {
+                                        // Show index properties if expanded
                                         for prop in &index.properties {
                                             ui.label(format!("Property: {:?}", prop));
                                         }
                                     });
+
+                                    // Handle toggling of index
+                                    if index_resp.header_response.clicked() {
+                                        if index_resp.body_response.is_some() {
+                                            // Index opened (expanded)
+                                            *selected_index = Some(index.clone());
+                                            if let Ok(new_doc_type) = contract
+                                                .contract
+                                                .document_type_cloned_for_name(&doc_name)
+                                            {
+                                                *selected_document_type = new_doc_type;
+                                                *selected_data_contract = contract.clone();
+                                                // Rebuild the query with the selected index
+                                                *document_query = format!(
+                                                    "SELECT * FROM {} WHERE `{}` = 'INSERT {} HERE'",
+                                                    selected_document_type.name(),
+                                                    index.property_names().first().expect("Expected the index to have at least one property name"),
+                                                    index.property_names().first().expect("Expected the index to have at least one property name")
+                                                );
+                                            }
+                                        } else {
+                                            // Index closed (collapsed)
+                                            *selected_index = None;
+                                            // Rebuild the query without index constraint
+                                            *document_query = format!(
+                                                "SELECT * FROM {}",
+                                                selected_document_type.name()
+                                            );
+                                        }
+                                    }
                                 }
                             });
+
+                            // Check doc type toggling
+                            if doc_resp.header_response.clicked() && doc_resp.body_response.is_some()
+                            {
+                                if let Ok(new_doc_type) =
+                                    contract.contract.document_type_cloned_for_name(&doc_name)
+                                {
+                                    *selected_document_type = new_doc_type;
+                                    *selected_data_contract = contract.clone();
+                                    *selected_index = None;
+                                    *document_query = format!(
+                                        "SELECT * FROM {}",
+                                        selected_document_type.name()
+                                    );
+                                }
+                            } else if doc_resp.header_response.clicked()
+                                && doc_resp.body_response.is_none()
+                            {
+                                // Doc type collapsed again: still have doc type & contract
+                                // required, so do not clear them. Just clear index if any.
+                                *selected_index = None;
+                                *document_query = format!(
+                                    "SELECT * FROM {}",
+                                    selected_document_type.name()
+                                );
+                            }
                         }
                     });
 
-                    ui.add_space(5.0); // Spacing between contracts
+                    ui.add_space(5.0);
                 }
             });
         });
--- src/ui/contracts_documents/document_query_screen.rs
+++ src/ui/contracts_documents/document_query_screen.rs
@@ -2,6 +2,7 @@ use crate::app::{AppAction, DesiredAppAction};
 use crate::backend_task::document::DocumentTask::FetchDocuments;
 use crate::backend_task::BackendTask;
 use crate::context::AppContext;
+use crate::model::qualified_contract::QualifiedContract;
 use crate::ui::components::contract_chooser_panel::add_contract_chooser_panel;
 use crate::ui::components::left_panel::add_left_panel;
 use crate::ui::components::top_panel::add_top_panel;
@@ -10,10 +11,10 @@ use crate::utils::parsers::{DocumentQueryTextInputParser, TextInputParser};
 use chrono::{DateTime, Utc};
 use dash_sdk::dpp::data_contract::accessors::v0::DataContractV0Getters;
 use dash_sdk::dpp::data_contract::document_type::accessors::DocumentTypeV0Getters;
-use dash_sdk::dpp::data_contract::document_type::DocumentType;
+use dash_sdk::dpp::data_contract::document_type::{DocumentType, Index};
 use dash_sdk::dpp::prelude::TimestampMillis;
-use dash_sdk::platform::{DataContract, Document};
-use egui::{Color32, Context, RichText, ScrollArea, TextEdit, Ui};
+use dash_sdk::platform::Document;
+use egui::{Color32, Context, Frame, Margin, RichText, ScrollArea, TextEdit, Ui};
 use std::sync::Arc;
 use std::time::{SystemTime, UNIX_EPOCH};
 
@@ -22,8 +23,9 @@ pub struct DocumentQueryScreen {
     error_message: Option<(String, MessageType, DateTime<Utc>)>,
     contract_search_term: String,
     document_query: String,
-    selected_data_contract: DataContract,
+    selected_data_contract: QualifiedContract,
     selected_document_type: DocumentType,
+    selected_index: Option<Index>,
     matching_documents: Vec<Document>,
     document_query_status: DocumentQueryStatus,
 }
@@ -37,17 +39,24 @@ pub enum DocumentQueryStatus {
 
 impl DocumentQueryScreen {
     pub fn new(app_context: &Arc<AppContext>) -> Self {
-        let selected_document_type = app_context
-            .dpns_contract
+        let dpns_contract = QualifiedContract {
+            contract: Arc::clone(&app_context.dpns_contract).as_ref().clone(),
+            alias: Some("dpns".to_string()),
+        };
+
+        let selected_document_type = dpns_contract
+            .contract
             .document_type_cloned_for_name("domain")
             .expect("Expected to find domain document type in DPNS contract");
+
         Self {
             app_context: app_context.clone(),
             error_message: None,
             contract_search_term: String::new(),
             document_query: format!("SELECT * FROM {}", selected_document_type.name()),
-            selected_data_contract: (*app_context.dpns_contract).clone(),
+            selected_data_contract: dpns_contract,
             selected_document_type,
+            selected_index: None,
             matching_documents: vec![],
             document_query_status: DocumentQueryStatus::NotStarted,
         }
@@ -72,14 +81,14 @@ impl DocumentQueryScreen {
     fn show_input_field(&mut self, ui: &mut Ui) -> AppAction {
         let mut action = AppAction::None;
         ui.horizontal(|ui| {
-            ui.label("Document SQL query: ");
             ui.text_edit_singleline(&mut self.document_query);
-            let button = egui::Button::new(RichText::new("Go").color(Color32::WHITE))
+            let button = egui::Button::new(RichText::new("Fetch Documents").color(Color32::WHITE))
                 .fill(Color32::from_rgb(0, 128, 255))
                 .frame(true)
                 .rounding(3.0);
             if ui.add(button).clicked() {
-                let parser = DocumentQueryTextInputParser::new(self.selected_data_contract.clone());
+                let parser =
+                    DocumentQueryTextInputParser::new(self.selected_data_contract.contract.clone());
                 match parser.parse_input(&self.document_query) {
                     Ok(parsed_query) => {
                         // Set the status to waiting and capture the current time
@@ -112,7 +121,6 @@ impl DocumentQueryScreen {
 
     fn show_output(&mut self, ui: &mut Ui) {
         ui.separator();
-        ui.label("Matching documents:");
         ui.add_space(10.0);
 
         ScrollArea::vertical().show(ui, |ui| {
@@ -131,15 +139,13 @@ impl DocumentQueryScreen {
                     ));
                 }
                 DocumentQueryStatus::Complete => {
-                    // Display fetched documents in JSON format (stored in self.matching_documents)
-                    // First, convert documents to JSON strings
-                    let mut json_string_documents =
-                        self.matching_documents
-                            .iter()
-                            .fold(String::new(), |acc, doc| {
-                                let doc_json = serde_json::to_string_pretty(doc).unwrap();
-                                format!("{}\n{}", acc, doc_json)
-                            });
+                    let docs: Vec<String> = self
+                        .matching_documents
+                        .iter()
+                        .map(|doc| serde_json::to_string_pretty(doc).unwrap())
+                        .collect();
+
+                    let mut json_string_documents = docs.join("\n\n");
 
                     ui.add(
                         TextEdit::multiline(&mut json_string_documents)
@@ -148,6 +154,7 @@ impl DocumentQueryScreen {
                             .font(egui::TextStyle::Monospace),
                     );
                 }
+
                 DocumentQueryStatus::ErrorMessage(ref message) => {
                     self.error_message =
                         Some((message.to_string(), MessageType::Error, Utc::now()));
@@ -206,13 +213,26 @@ impl ScreenLike for DocumentQueryScreen {
             RootScreenType::RootScreenDocumentQuery,
         );
 
-        action |=
-            add_contract_chooser_panel(ctx, &mut self.contract_search_term, &self.app_context);
+        action |= add_contract_chooser_panel(
+            ctx,
+            &mut self.contract_search_term,
+            &self.app_context,
+            &mut self.selected_data_contract,
+            &mut self.selected_document_type,
+            &mut self.selected_index,
+            &mut self.document_query,
+        );
 
-        egui::CentralPanel::default().show(ctx, |ui| {
-            action |= self.show_input_field(ui);
-            self.show_output(ui);
-        });
+        egui::CentralPanel::default()
+            .frame(
+                Frame::none()
+                    .fill(ctx.style().visuals.panel_fill)
+                    .inner_margin(Margin::same(10.0)),
+            )
+            .show(ctx, |ui| {
+                action |= self.show_input_field(ui);
+                self.show_output(ui);
+            });
 
         action
     }

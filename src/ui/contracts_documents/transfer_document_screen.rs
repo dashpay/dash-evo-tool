5e0d1364de240e1fcfa50871fc7b688067cabbe3
pauldelucia <pauldelucia2@gmail.com>
2025-05-27 19:23:48 +0700

    fix

--- src/ui/contracts_documents/transfer_document_screen.rs
+++ src/ui/contracts_documents/transfer_document_screen.rs
@@ -293,7 +293,10 @@ impl ScreenLike for TransferDocumentScreen {
                 return;
             }
 
+            ui.add_space(10.0);
             ui.separator();
+            ui.add_space(10.0);
+
             ui.heading("2. Select an identity and key:");
             ui.add_space(10.0);
             self.ui_identity_picker(ui);
@@ -307,20 +310,26 @@ impl ScreenLike for TransferDocumentScreen {
                 }
             }
 
+            ui.add_space(10.0);
             ui.separator();
+            ui.add_space(10.0);
+
             ui.heading("3. Enter the Document ID to transfer:");
-            ui.add_space(5.0);
+            ui.add_space(10.0);
             ui.text_edit_singleline(&mut self.doc_id_input);
 
+            ui.add_space(10.0);
             ui.separator();
+            ui.add_space(10.0);
+
             ui.heading("4. Enter the Recipient ID:");
-            ui.add_space(5.0);
+            ui.add_space(10.0);
             ui.text_edit_singleline(&mut self.recipient_id_input);
 
-            ui.add_space(10.0);
             // Display token costs if any
             if let Some(doc_type) = &self.selected_doc_type {
                 if let Some(token_creation_cost) = doc_type.document_transfer_token_cost() {
+                    ui.add_space(10.0);
                     let token_amount = token_creation_cost.token_amount;
                     let token_name = token_creation_cost
                         .contract_id

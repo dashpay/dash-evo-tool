d493efdf159f5bc2b9142f9f90e2f02c8e9c6136
pauldelucia <pauldelucia2@gmail.com>
2025-10-21 17:25:45 +0700

    apply suggestion

--- build.rs
+++ build.rs
@@ -1,18 +1,4 @@
-use std::env;
-use std::fs;
-use std::path::Path;
-
 fn main() {
-    // Generate version info
-    let out_dir = env::var("OUT_DIR").unwrap();
-    let version_file = Path::new(&out_dir).join("version.rs");
-    let version = env::var("CARGO_PKG_VERSION").unwrap();
-    fs::write(
-        version_file,
-        format!("pub const VERSION: &str = \"{}\";", version),
-    )
-    .unwrap();
-
     // Windows-specific icon embedding
     #[cfg(target_os = "windows")]
     {

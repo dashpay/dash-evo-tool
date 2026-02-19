e9db82a1ed80c28c01f6bb4df0ab64e871a3b8d8
Lukasz Klimek <842586+lklimek@users.noreply.github.com>
2026-02-19 11:15:48 +0100

    fmt

--- src/ui/wallets/wallets_screen/address_table.rs
+++ src/ui/wallets/wallets_screen/address_table.rs
@@ -194,8 +194,7 @@ impl WalletsBalancesScreen {
 
         if !self.show_zero_balance_addresses {
             address_data.retain(|data| {
-                let is_platform_payment =
-                    data.account_category == AccountCategory::PlatformPayment;
+                let is_platform_payment = data.account_category == AccountCategory::PlatformPayment;
                 if data.account_category.is_key_only() {
                     true
                 } else if is_platform_payment {
